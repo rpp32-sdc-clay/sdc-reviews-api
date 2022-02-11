71d88b24bb1584dad550b24f6e7ab7e9b9b4e58a
Chelsea Lipson <chelseamlipson@gmail.com>
2022-02-11 13:49:44 -0800

    Added more express routes for API.

--- api.js
+++ api.js
@@ -2,9 +2,31 @@ const express = require('express');
 const app = express()
 const port = 3000
 
+
+//get all reviews
 app.get('/reviews', (req, res) => {
+
 })
 
+//get meta review info for individual products
+app.get('/reviews/meta/:product_id', (req, res) => {
+
+})
+
+//post new review
+app.post('/reviews', (req, res) => {
+
+})
+
+//mark review as helpful
+app.put('/reviews/:review_id/helpful', (req, res) => {
+
+})
+
+//mark review as reported
+app.put('/reviews/:review_id/report', (req, res) => {
+
+})
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
