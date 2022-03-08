41cf40593f214576d00d0b8ba9f527e6831408ab
PixelizedChell <chelseamlipson@gmail.com>
2022-03-08 04:46:42 +0000

    Added loader.io verification

--- api.js
+++ api.js
@@ -86,6 +86,12 @@ app.put('/reviews/:review_id/report', (req, res) => {
   })
 })
 
+
+app.get('/loaderio-b3ac7883eee3add2a5bb8df726086025', (req, res) => {
+  //loader.io route
+  res.sendFile(__dirname + '/loaderio-b3ac7883eee3add2a5bb8df726086025.txt')
+})
+
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
 })
